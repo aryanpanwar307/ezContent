33a9256782cbeeba827de8d57902d41c726d8df3
aryanpanwar307 <aryan.panwar.307.ap@gmail.com>
2025-11-04 01:35:38 +0530

    "added debugging"

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def generate_content():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
